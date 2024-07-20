b116a6d8f62e13155e08f0616de436646f05d772
Mannas <mannasnarang8@gmail.com>
2024-07-20 12:19:32 +0530

    update

--- app.ts
+++ app.ts
@@ -1,6 +1,17 @@
 import express from "express"
 const app = express();
 
+
+app.post('/login', async (req,res) => {
+    const details = await req.body();
+    try {
+        
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
 app.listen(6969, () => {
     console.log("server started")
 })
